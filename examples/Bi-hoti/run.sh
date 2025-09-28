c57026511e3a0fcda94b65bf99c8c850922c3a40
hvakili0 <hvakilitaleghani2@nebraska.edu>
2025-09-27 22:52:28 -0500

    smaller2

--- examples/Bi-hoti/run.sh
+++
@@ -1,2 +0,0 @@
-irrep -spinor   -code=vasp   -kpnames=T,GM,F,L   -Ecut=50   -refUC=1,-1,0,0,1,-1,1,1,1   -EF=5.2156   -IBstart=5   -IBend=10 > out
-rm trace.txt irreps.dat irreptable-template
--- examples/vasp_spinor/run.sh
+++
@@ -1,2 +0,0 @@
-irrep -spinor -Ecut=50 -code=vasp -kpoints=1 -kpnames="GM" > out
-rm trace.txt irreps.dat irreptable-template
